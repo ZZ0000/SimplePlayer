2c604c95692c4e18bc25dc2bda6c9c1cb55f886d
arthur.svezhentsev <java@training.workrocks.ua>
2016-12-19 20:48:25 +0200

    first commit

---
+++ app/src/main/java/com/levup/simpleplayer/presenters/SongsPresenter.java
@@ -0,0 +1,45 @@
+package com.levup.simpleplayer.presenters;
+
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+import com.levup.simpleplayer.models.Song;
+import com.levup.simpleplayer.views.SongsView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by java on 05.12.2016.
+ */
+
+public class SongsPresenter {
+
+    private SongsView mView = null;
+
+    public void onAttachToView(@NonNull SongsView songsView) {
+        mView = songsView;
+    }
+
+    private Subscription subscription = null;
+
+    public void loadAllSongs() {
+        subscription = Observable.just(SongsRepository.getAllSongs(mView.getContext()))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(songs -> { mView.onAllSongsLoaded(songs);},
+                        Throwable::printStackTrace);
+
+    }
+
+    public void onDetach() {
+        if(subscription != null)
+            subscription.unsubscribe();
+    }
+
+}
---
+++ app/src/main/java/com/levup/simpleplayer/services/PlayBackService.java
@@ -0,0 +1,159 @@
+package com.levup.simpleplayer.services;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.view.Surface;
+import android.widget.Toast;
+
+import com.levup.simpleplayer.BuildConfig;
+import com.levup.simpleplayer.models.Song;
+import com.levup.simpleplayer.presenters.SongsRepository;
+import com.levup.simpleplayer.views.MainActivity;
+import com.levup.simpleplayer.R;
+
+public class PlayBackService extends Service implements MediaPlayer.OnPreparedListener {
+
+    public static final String ACTION_PLAY = BuildConfig.APPLICATION_ID + ".action.PLAY";
+
+    public static final String TAG = PlayBackService.class.getSimpleName();
+    private static final int NOTIFICATION_ID = 101;
+
+    private final IBinder mBinder = new PlayBackBinder();
+
+    private MediaPlayer mMediaPlayer = null;
+
+    public static Intent newInstance(Context context) {
+        return new Intent(context, PlayBackService.class);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate()");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand(" + intent.getAction()+")");
+        if(intent.getAction() == null) return Service.START_STICKY;
+        if (intent.getAction().equals(ACTION_PLAY)) {
+            try {
+                mMediaPlayer = new MediaPlayer();
+                mMediaPlayer.setDataSource(this, getSongs());
+                mMediaPlayer.setOnPreparedListener(this);
+               // mMediaPlayer.prepareAsync();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Service.START_STICKY;
+    }
+
+    private Uri getSongs() {
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if (cursor == null) {
+            // query failed, handle error.
+        } else if (!cursor.moveToFirst()) {
+            // no media on the device
+        } else {
+            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            do {
+                long thisId = cursor.getLong(idColumn);
+                String thisTitle = cursor.getString(titleColumn);
+                Uri contentUri = ContentUris.withAppendedId(
+                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        thisId);
+
+                return contentUri;
+                // ...process entry...
+            } while (cursor.moveToNext());
+        }
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy()");
+    }
+
+    /*@Override
+    public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "onUnbind()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onUnbind()");
+        return super.onUnbind(intent);
+    }*/
+
+    public PlayBackService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mMediaPlayer.start();
+
+        PendingIntent pi = PendingIntent.getActivity(
+                getApplicationContext(),
+                0,
+                new Intent(getApplicationContext(), MainActivity.class),
+                PendingIntent.FLAG_NO_CREATE);
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setContentIntent(pi);
+
+        startForeground(NOTIFICATION_ID, builder.build());
+    }
+
+    public void playSongId(long songId) {
+       // Song song = SongsRepository.getSongForID(this, songId);
+        Uri contentUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                songId);
+        try {
+            if(mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+                mMediaPlayer.stop();
+                mMediaPlayer.release();
+                mMediaPlayer = null;
+            }
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(this, contentUri);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class PlayBackBinder extends Binder {
+        public PlayBackService getService() {
+            return PlayBackService.this;
+        }
+    }
+
+}
